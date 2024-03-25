4ffde628a4dcb7fd6f8948e37a618610c6c1b17f
Alex <avtodaro@gmail.com>
2024-03-25 16:47:16 -0700

    Fix indentation

--- PhobGCC/common/phobGCC.h
+++ PhobGCC/common/phobGCC.h
@@ -1681,9 +1681,9 @@ void processButtons(Pins &pin, Buttons &btn, Buttons &hardware, ControlConfig &c
 		case 6: //Scales Analog Trigger Values
 			tempBtn.La = (uint8_t) readLa(pin, controls.lTrigInitial, triggerScaleL) * shutoffLa;
 			break;
-    case 7: //Trigger Trick Emulation state
-      tempBtn.La = (uint8_t) max(0, (readLa(pin, controls.lTrigInitial, 1) * shutoffLa) - (controls.lTriggerOffset - controls.triggerMin));
-      break;
+		case 7: //Trigger Trick Emulation state
+			tempBtn.La = (uint8_t) max(0, (readLa(pin, controls.lTrigInitial, 1) * shutoffLa) - (controls.lTriggerOffset - controls.triggerMin));
+			break;
 		default:
 			tempBtn.La = (uint8_t) readLa(pin, controls.lTrigInitial, 1) * shutoffLa;
 	}
@@ -1724,9 +1724,9 @@ void processButtons(Pins &pin, Buttons &btn, Buttons &hardware, ControlConfig &c
 		case 6: //Scales Analog Trigger Values
 			tempBtn.Ra = (uint8_t) readRa(pin, controls.rTrigInitial, triggerScaleR) * shutoffRa;
 			break;
-    case 7: //Trigger Trick Emulation state
-      tempBtn.Ra = (uint8_t) max(0, (readRa(pin, controls.rTrigInitial, 1) * shutoffRa) - (controls.rTriggerOffset - controls.triggerMin));
-      break;
+		case 7: //Trigger Trick Emulation state
+			tempBtn.Ra = (uint8_t) max(0, (readRa(pin, controls.rTrigInitial, 1) * shutoffRa) - (controls.rTriggerOffset - controls.triggerMin));
+			break;
 		default:
 			tempBtn.Ra = (uint8_t) readRa(pin, controls.rTrigInitial, 1) * shutoffRa;
 	}
